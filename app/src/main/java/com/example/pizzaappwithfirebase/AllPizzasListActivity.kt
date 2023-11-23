05244304b04c3891d5f2e488957d202df2b068d7
Muindo N. Gituku <ngeenegituku@gmail.com>
2023-11-22 19:23:32 -0500

    Final Setup

---
+++ app/src/main/java/com/example/pizzaappwithfirebase/AllPizzasListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pizzaappwithfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllPizzasListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_pizzas_list)
+    }
+}
